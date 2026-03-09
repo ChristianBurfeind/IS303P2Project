72fdff5e4e2a2f3ab2593f72a590edbd8cece8db
quintanaalan13-pixel <alanlq@byu.edu>
2026-03-08 21:21:33 -0600

    Create initial_code_soccer_teams.py

---
+++ initial_code_soccer_teams.py
@@ -0,0 +1,9 @@
+"""
+- Display the final record for a team.
+- Receive the home team data
+- Parameters = sHomeTeam, iWins, iLoss
+- display information.
+- print(f “{sHomeTeam} finished with a record of {iWins}-{iLoss}”)"""
+
+def finishSeason(sHomeTeam, iWins, iLoss):
+    print(f"{sHomeTeam} finished with a record of {iWins} - {iLoss}")
